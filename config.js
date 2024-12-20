7180965c19d4a9e12c6a142d7b75b9b8329deb31
Ali Ahmad Farooq <aliahmad747426@gmail.com>
2024-12-20 22:24:05 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "jiAhhb5K#JqnjjeScjAWoqGlhEBMWqy6BmgS_qIUOyic5r6sodkY",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE  === undefined ? 'false' : process.env.AUTO_VOICE, 
+AUTO_VOICE: process.env.AUTO_VOICE || "true", 
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/KFYzSLs/Picsart-24-11-23-16-00-15-845.jpg",
